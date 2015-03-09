20601b79c0ff4cf7da4a617d46c3504ee555a906
swzhou <swing.zhou@hotmail.com>
2015-03-09 22:27:51 +0800

    [swzhou] routing

--- AspnetWebapiBootcamp/App_Start/WebApiConfig.cs
+++ AspnetWebapiBootcamp/App_Start/WebApiConfig.cs
@@ -17,7 +17,13 @@ namespace AspnetWebapiBootcamp
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new {id = RouteParameter.Optional}
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new {id = RouteParameter.Optional}
             );
         }
     }
---
+++ AspnetWebapiBootcamp/Controllers/ActionsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+
+namespace AspnetWebapiBootcamp.Controllers
+{
+    public class ActionsController: ApiController
+    {
+        [AcceptVerbs("PUT", "DELETE")]
+        public string AvailableActions()
+        {
+            return "all";
+        }
+
+        [HttpGet]
+        public string Details(int id)
+        {
+            return string.Format("Details of {0}", id);
+        }
+
+        [HttpGet]
+        [ActionName("name")]
+        public string GetName(int id)
+        {
+            return string.Format("Name of {0} is: Name.", id);
+        }
+
+        [NonAction]
+        public string PrivateData()
+        {
+            return "private data";
+        }
+    }
+}
